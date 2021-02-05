634975c513632875240c5269d2d57018c429b98c
keiselami <kei@aledia.ca>
2021-02-05 19:08:44 +0100

    Added classes for the EmployeeService

--- src/main/java/com/kei/kei/employee/repository/EmployeeRepository.java
+++ src/main/java/com/kei/kei/employee/repository/EmployeeRepository.java
@@ -4,7 +4,11 @@ import com.kei.kei.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long employeeId);
 
 }
--- src/main/java/com/kei/kei/employee/service/EmployeeService.java
+++ src/main/java/com/kei/kei/employee/service/EmployeeService.java
@@ -1,7 +1,38 @@
 package com.kei.kei.employee.service;
 
+import com.kei.kei.employee.model.Employee;
+import com.kei.kei.employee.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class EmployeeService {
+    @Autowired
+    private final EmployeeRepository employeeRepository;
+
+    public Employee store(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAll(){
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> findEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee update(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void delete(Long employeeId) {
+        employeeRepository.deleteEmployeeById(employeeId);
+    }
+
 }
